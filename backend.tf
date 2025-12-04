2b10f709326b5461988ebfbf765ebd8bda96db74
Michael Angelo Viola <mico@Michaels-MacBook-Pro.local>
2025-12-04 13:35:44 +0800

    ubpoc

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     encrypt = true    
-    bucket = "dynamic_env-gpoc-tfstate"
+    bucket = "dynamic_env-ubpoc-tfstate"
     #dynamodb_table = "dynamic_env-gpoc-tfstate-lock-dynamo"
     #key    = "terraform.tfstate"
     region = "ap-northeast-1"
